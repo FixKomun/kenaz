7de00251974381a072761ecb1e66ea9c77133d14
Filip Franić <fixkomun@gmail.com>
2020-11-12 21:26:24 +0100

    Fixed_final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
-  /*
+  const myModal = document.querySelector(".my-modal");
+  const images = document.querySelectorAll(".gallery img");
+  const enlarge = document.querySelector(".enlarge img");
+  const original = document.querySelector(".full-image");
+  const imgText = document.querySelector(".caption");
+  const now = document.querySelectorAll(".now");
+
+  enlarge.addEventListener("click", () => {
+    now.forEach((noww) => {
+      myModal.classList.add("open");
+      original.classList.add("open");
+      //Dynamic change text and image
+      images.forEach((image) => {
+        console.log(image);
+        if (noww.classList.contains("active")) {
+          const originalSrc = image.getAttribute("data-original");
+          original.src = `./images/${originalSrc}`;
+          console.log(originalSrc);
+          const imgTextSrc = image.getAttribute("alt");
+          imgText.textContent = imgTextSrc;
+        }
+      });
+    });
+  });
+
+  myModal.addEventListener("click", (e) => {
+    //console.log(e);
+    if (e.target.classList.contains("my-modal")) {
+      myModal.classList.remove("open");
+      original.classList.remove("open");
+    }
+  });
+});
+//images.forEach((image) => {});
+
+/*
   $(".after").on("click", function () {
     var currentImg = $(".active");
     var nextImg = currentImg.next();
@@ -27,10 +62,11 @@ $(document).ready(function () {
   $(".number-list .btn").on("click", function () {
     $(this).css("background-color", "purple");
   });
-*/
+
   $("h2.click").on("click", function () {
     activeButton = $(".clicked");
     activeButton.removeClass("clicked");
     $(this).addClass("clicked");
   });
-});
+
+*/
